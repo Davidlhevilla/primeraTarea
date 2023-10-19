1eff8594fd29bae0af7a952a7e74c25d7fd0002d
Davidlhevilla <92096052+Davidlhevilla@users.noreply.github.com>
2023-10-19 20:11:41 +0200

    Subida David

--- primera.Tarea/src/main/java/primera/Tarea/Main.java
+++ primera.Tarea/src/main/java/primera/Tarea/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		System.out.println("hola Nora");
 		System.out.println("Hola mama");
 		System.out.println("Hola Davilillo");
-		System.out.println("Hola papa");
+		System.out.println("Hola papa, como estas?");
 		
 
 	}
