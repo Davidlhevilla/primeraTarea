b02244ea4d5d14e11b4dc8a1bf30a5d969759bd0
CristinaConesa <148486340+CristinaConesa@users.noreply.github.com>
2023-10-19 20:03:46 +0200

    primeraSubida

---
+++ primera.Tarea/src/main/java/primera/Tarea/Main.java
@@ -0,0 +1,17 @@
+package primera.Tarea;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		System.out.println("hola Nora");
+		System.out.println("Hola mama");
+		System.out.println("Hola Davilillo");
+		System.out.println("Hola papá");
+		
+
+	}
+
+}
